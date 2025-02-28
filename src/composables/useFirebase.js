b1c6ca3761668313afbc1eae47439835ef11ad00
Kevin Dunn <kdunn1@stu.southtexascollege.edu>
2025-02-27 19:02:34 -0600

    added cards, installed firebase

---
+++ src/composables/useFirebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyALSWAwxX2wArIdtbp-1fNdm2Z1W-DSKtQ",
+  authDomain: "project5-2e0f1.firebaseapp.com",
+  projectId: "project5-2e0f1",
+  storageBucket: "project5-2e0f1.firebasestorage.app",
+  messagingSenderId: "418378823119",
+  appId: "1:418378823119:web:aee29357c3a9bf4acd0eba"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
